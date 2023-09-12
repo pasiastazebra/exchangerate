8124e2df7c76babc7924bc9c88faaae08c30c828
pasiastazebra <vobis08gw@gmail.com>
2023-09-12 23:24:14 +0200

    dom getters done

---
+++ src/scripts/main.js
@@ -0,0 +1,8 @@
+//DOM elements
+const fromMenu = document.getElementById(from);
+const fromValue = document.getElementById(from-value);
+
+const toMenu = document.getElementById(to);
+const toValue = document.getElementById(to-value);
+
+const submitButton = document.getElementById(submit);
