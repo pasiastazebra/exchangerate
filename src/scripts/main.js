05b44c9bc5f3ed3d20de8508c6492bf712a5c4f6
pasiastazebra <vobis08gw@gmail.com>
2023-09-13 23:34:58 +0200

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,4 +13,5 @@ const apiKey = '95d27d6cedc8c320cba92066';
 
 const getData = (key, valueOne, valueTwo) => {
     const response = fetch(`https://v6.exchangerate-api.com/v6/${key}/pair/${valueOne}/${valueTwo}`);
+    return response;
 }
