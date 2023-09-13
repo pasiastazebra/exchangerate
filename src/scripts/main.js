0bfb34f657d7615b3e1aaa1101545fd3b630bda0
pasiastazebra <vobis08gw@gmail.com>
2023-09-13 23:30:29 +0200

    API statements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,11 @@ const toMenu = document.getElementById(to);
 const toValue = document.getElementById(to-value);
 
 const submitButton = document.getElementById(submit);
+
+//API
+
+const apiKey = '95d27d6cedc8c320cba92066';
+
+const getData = (key, valueOne, valueTwo) => {
+    const response = fetch(`https://v6.exchangerate-api.com/v6/${key}/pair/${valueOne}/${valueTwo}`);
+}
