a8740ef171c8ddb12d504c44c974f6e36781a6f0
pasiastazebra <vobis08gw@gmail.com>
2023-09-21 13:14:11 +0200

    API pinging working

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,26 @@
 //DOM elements
-const fromMenu = document.getElementById(from);
-const fromValue = document.getElementById(from-value);
+const fromMenu = document.getElementById("from");
+const fromValue = document.getElementById("from-value");
 
-const toMenu = document.getElementById(to);
-const toValue = document.getElementById(to-value);
+const toMenu = document.getElementById("to");
+const toValue = document.getElementById("to-value");
 
-const submitButton = document.getElementById(submit);
+const submitButton = document.getElementById("submit");
 
 //API
 
 const apiKey = '95d27d6cedc8c320cba92066';
 
-const getData = (key, valueOne, valueTwo) => {
-    const response = fetch(`https://v6.exchangerate-api.com/v6/${key}/pair/${valueOne}/${valueTwo}`);
-    return response;
+const getData = (valueOne, valueTwo, key = apiKey) => {
+
+    return fetch(`https://v6.exchangerate-api.com/v6/${key}/pair/${valueOne}/${valueTwo}`).then( ( respond ) => { return respond.json() } );
+
 }
+
+async function logData() {
+    const logTest = await getData("PLN", "USD");
+
+    console.log(logTest);
+}
+
+logData();
